92d48e79f4da152c4cdf7590b99b26998fffbe1b
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2026-01-28 19:18:42 +0100

    Adding client

---
+++ EmaClient/src/main/java/ema/maven/model/Alumno.java
@@ -0,0 +1,44 @@
+package ema.maven.model;
+
+public class Alumno {
+	private int id;
+	private String nombre;
+	private String curso;
+	
+	public Alumno() {}
+	
+	public Alumno(int id, String nombre, String curso) {
+		this.id = id;
+		this.nombre = nombre;
+		this.curso = curso;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumnos [id=" + id + ", nombre=" + nombre + ", curso=" + curso + "]";
+	}
+}
