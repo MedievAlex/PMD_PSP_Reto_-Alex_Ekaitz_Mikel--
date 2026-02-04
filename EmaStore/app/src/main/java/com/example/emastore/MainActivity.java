e2744d6bbe3d4f6763d83249a4c2a095c6da21e5
MedievAlex <alexirazola@gmail.com>
2026-02-04 13:04:16 +0100

    Upadated audio method

--- EmaStore/app/src/main/java/com/example/emastore/MainActivity.java
+++ EmaStore/app/src/main/java/com/example/emastore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.emastore;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,11 +34,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(btnAudio.getText().equals(R.string.mute_audio)) {
                     btnAudio.setText(R.string.unmute_audio);
+                    audioPlayer(true);
                 } else if(btnAudio.getText().equals(R.string.unmute_audio)) {
                     btnAudio.setText(R.string.mute_audio);
+                    audioPlayer(false);
                 }
             }
         });
+        audioPlayer(true);
     }
 
     private void initViews() {
@@ -102,5 +106,14 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+    }
+    private void audioPlayer(boolean isPlaying){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.background_music);
+
+        if(isPlaying) {
+            mediaPlayer.start();
+        } else {
+            mediaPlayer.pause();
+        }
     }
 }
